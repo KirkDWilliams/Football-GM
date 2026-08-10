9c55ac86f133bda26572c58737efc76266484d36
Kirk Williams <williams.kirk.d@gmail.com>
2026-08-09 22:32:05 -0400

    added comments for Data loader

--- api/Data/Entity/InjuryStatus.cs
+++ api/Data/Entity/InjuryStatus.cs
@@ -2,7 +2,7 @@ namespace FootballGm.Api.Data.Entity
 {
     public class InjuryStatus // load_injuries()
     {
-        public short Season { get; set; } = 0;                            // Season
+        public short Season { get; set; } = 0;                            // season
         public short Week { get; set; } = 0;                              // week
         public string PlayerId { get; set; } = string.Empty;              // gsis_id
         public string OfficialReportStatus { get; set; } = string.Empty;  // report_primary_injury
@@ -11,3 +11,19 @@ namespace FootballGm.Api.Data.Entity
         public string LastUpdated { get; set; } = string.Empty;           // date_modified
     }
 }
+
+/*   
+    R
+ 
+    all_injuries <- load_injuries(2026)
+
+    injuries <- data.frame(
+	    GameId = all_injuries$season,
+	    Week = all_injuries$week,
+	    PlayerId = all_injuries$gsis_id,
+	    OfficialReportStatus = all_injuries$report_primary_injury,
+	    PracticePrimaryStatus = all_injuries$practice_primary_injury,
+	    PracticeStatus = all_injuries$practice_status
+    )
+ 
+ */
--- api/Data/Entity/Player.cs
+++ api/Data/Entity/Player.cs
@@ -11,3 +11,27 @@ namespace FootballGm.Api.Data.Entity
         public short DraftYear { get; set; }                    // rookie_season
     }
 }
+/*
+ * R
+    install.packages(c("DBI", "RSQLite", "nflreadr"))
+
+    library(DBI)
+    library(RSQLite)
+    library(nflreadr)
+
+    all_player_summary <- load_players()
+
+    off_player_info <- all_player_summary[
+        all_player_summary$position %in% c("QB", "RB", "WR", "TE", "K", "P"),
+    ]
+
+    player_info <- data.frame(
+	    PlayerId = off_player_info$gsis_id,
+	    Name = off_player_info$display_name,
+	    PictureUrl = off_player_info$headshot,
+	    Team = off_player_info$latest_team,
+	    Position = off_player_info$position,
+	    JerseyNumber = off_player_info$jersey_number,
+	    DraftYear = off_player_info$rookie_season
+    )
+ */
