2b5150ede72d1e5fa047c0cedfe22c1291cf6f5b
Kirk Williams <williams.kirk.d@gmail.com>
2026-09-10 22:47:39 -0400

    specified actions after minutesUntil functions. Split out the library connections into a text file

--- api/Services/BackgroundServices/MasterBackgroundService.cs
+++ api/Services/BackgroundServices/MasterBackgroundService.cs
@@ -1,121 +1,174 @@
-namespace FootballGm.Api.Services.BackgroundServices
+namespace FootballGm.Api.Services.BackgroundServices;
+
+public class MasterBackgroundService : BackgroundService
 {
-    public class MasterBackgroundService : BackgroundService
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly ILogger<MasterBackgroundService> _logger;
+    private readonly TimeSpan _checkInterval = TimeSpan.FromMinutes(10);
+
+    public MasterBackgroundService(IServiceScopeFactory scopeFactory, ILogger<MasterBackgroundService> logger)
     {
-        private readonly IServiceScopeFactory _scopeFactory;
-        private readonly ILogger<MasterBackgroundService> _logger;
-        private readonly TimeSpan _checkInterval = TimeSpan.FromMinutes(10);
-    
-        public MasterBackgroundService(IServiceScopeFactory scopeFactory, ILogger<MasterBackgroundService> logger)
-        {
-            _scopeFactory = scopeFactory;
-            _logger = logger;
-        }
+        _scopeFactory = scopeFactory;
+        _logger = logger;
+    }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            _logger.LogInformation("Background services running.");
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Background services running.");
 
-            while (!stoppingToken.IsCancellationRequested)
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
             {
-                try
+                var currentDay = DateTime.Now;
+
+                var nflWeek = Helpers.WeekHelper.CurrentWeek;
+
+                switch (currentDay.DayOfWeek)
                 {
-                    var currentDay = DateTime.Now;
+                    case DayOfWeek.Sunday:
+                    case DayOfWeek.Monday:
+                        await MinutesUntil(desiredHour: 1, desiredMinute: 0,
+                            async game => await UpdateGameStats(), stoppingToken);
 
-                    var nflWeek = Helpers.WeekHelper.CurrentWeek;
+                        await FinishDay(stoppingToken);
+                        break;
 
-                    switch (currentDay.DayOfWeek)
-                    {
-                        case DayOfWeek.Sunday:
-                        case DayOfWeek.Monday:
-                            await MinutesUntil(desiredHour: 1, desiredMinute: 0, stoppingToken);
-                            // pull game stats
-                            await FinishDay(stoppingToken);
-                            break;
+                    case DayOfWeek.Tuesday:
+                        await MinutesUntil(desiredHour: 1, desiredMinute: 0,
+                            async game => await UpdateGameStats(), stoppingToken);
 
-                        case DayOfWeek.Tuesday:
-                            await MinutesUntil(desiredHour: 1, desiredMinute: 0, stoppingToken);
-                            // pull game stats
+                        await MinutesUntil(desiredHour: 6, desiredMinute: 0,
+                            async match => await DetermineMatches(nflWeek), stoppingToken);
 
-                            await MinutesUntil(desiredHour: 6, desiredMinute: 0, stoppingToken);
-                            // Determine who won each game
-
-                            await MinutesUntil(desiredHour: 9, desiredMinute: 0, stoppingToken);
-                            // Open Auction Free Agency
-                            // Open Trading
-
-                            await FinishDay(stoppingToken);
-                            break;
-
-                        case DayOfWeek.Wednesday:
-                            await MinutesUntil(desiredHour: 9, desiredMinute: 0, stoppingToken);
-                            // Close free agency auction
-                            // Award players to teams and update budgets
-                            // Open Free Agency Contracts
-
-                            if (nflWeek == 12)
+                        await MinutesUntil(desiredHour: 9, desiredMinute: 0,
+                            async agency =>
                             {
-                                /* thanksgiving slide */
-                                await MinutesUntil(desiredHour: 19, desiredMinute: 0, stoppingToken);
-                                // close free agency
-                                // close trading
-                                // award free agency
-                            }
+                                await OpenAuctionedFreeAgency();
+                                await OpenTrading(); }, stoppingToken);
+                        // Open Trading
 
+                        await FinishDay(stoppingToken);
+                        break;
+
+                    case DayOfWeek.Wednesday:
+                        await MinutesUntil(desiredHour: 1, desiredMinute: 0, async game => await UpdateGameStats(), stoppingToken);
+                        if (nflWeek == 12)
+                        {
+                            /* thanksgiving slide */
+                            await MinutesUntil(desiredHour: 10, desiredMinute: 0, async k =>
+                            {
+                                CloseAuctions();
+                                CloseUnrestrictedFreeAgency();
+                                CloseTrading();
+                            }, stoppingToken);
+                            
                             await FinishDay(stoppingToken);
-                            break;
+                        }
 
-                        case DayOfWeek.Thursday:
-                            await MinutesUntil(desiredHour: 1, desiredMinute: 0, stoppingToken);
-                            // pull game stats
+                        await MinutesUntil(desiredHour: 12, desiredMinute: 0,
+                            async k => CloseAuctions(), stoppingToken);
+                        await MinutesUntil(desiredHour: 1, desiredMinute: 0,
+                            async k => OpenUnrestrictedFreeAgency(), stoppingToken);
 
-                            if (nflWeek == 12)
-                                await FinishDay(stoppingToken);
+                        await FinishDay(stoppingToken);
+                        break;
 
-                            await MinutesUntil(desiredHour: 19, desiredMinute: 0, stoppingToken);
-                            // close free agency
-                            // close trading
-                            // award free agency
+                    case DayOfWeek.Thursday:
+                        await MinutesUntil(desiredHour: 1, desiredMinute: 0,
+                            async game => await UpdateGameStats(), stoppingToken);
 
+                        if (nflWeek == 12)
                             await FinishDay(stoppingToken);
-                            break;
 
-                        case DayOfWeek.Friday:
-                        case DayOfWeek.Saturday:
-                            await MinutesUntil(desiredHour: 1, desiredMinute: 0, stoppingToken);
-                            // pull game stats
-                            await FinishDay(stoppingToken);
-                            break;
-                    }
+                        await MinutesUntil(desiredHour: 19, desiredMinute: 0, async k =>
+                        {
+                            CloseAuctions();
+                            CloseUnrestrictedFreeAgency();
+                            CloseTrading();
+                        }, stoppingToken);
+
+                        await FinishDay(stoppingToken);
+                        break;
+
+                    case DayOfWeek.Friday:
+                    case DayOfWeek.Saturday:
+                        await MinutesUntil(desiredHour: 1, desiredMinute: 0, async game => await UpdateGameStats(), stoppingToken);
+                        // pull game stats
+                        await FinishDay(stoppingToken);
+                        break;
                 }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Error while closing expired auctions");
-                }
-
-                // Wait before the next check
-                await Task.Delay(_checkInterval, stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while closing expired auctions");
             }
 
-            _logger.LogInformation("AuctionCloserBackgroundService stopped");
+            // Wait before the next check
+            await Task.Delay(_checkInterval, stoppingToken);
         }
 
-        private async static Task MinutesUntil(int desiredHour, int desiredMinute, CancellationToken stoppingToken = default)
-        {
-            var currentTime = TimeOnly.FromDateTime(DateTime.Now);
-            if (currentTime > new TimeOnly(desiredHour, desiredMinute))
-                return;
+        _logger.LogInformation("AuctionCloserBackgroundService stopped");
+    }
 
-            var minutes = 60 * (desiredHour - currentTime.Hour)
-                - currentTime.Minute
-                + desiredMinute;
+    private async Task OpenTrading()
+    {
+        throw new NotImplementedException();
+    }
 
-            await Task.Delay(TimeSpan.FromMinutes(minutes), stoppingToken);
-        }
+    private void CloseTrading()
+    {
+        throw new NotImplementedException();
+    }
 
-        private async static Task FinishDay(CancellationToken stoppingToken)
-        {
-            await MinutesUntil(desiredHour: 23, desiredMinute: 55, stoppingToken);
-        }
+    private void CloseUnrestrictedFreeAgency()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OpenUnrestrictedFreeAgency()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CloseAuctions()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task OpenAuctionedFreeAgency()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task DetermineMatches(int nflWeek)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task UpdateGameStats()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async static Task MinutesUntil(int desiredHour, int desiredMinute, Func<CancellationToken, Task>? onComplete = null, CancellationToken stoppingToken = default)
+    {
+        var currentTime = TimeOnly.FromDateTime(DateTime.Now);
+        if (currentTime > new TimeOnly(desiredHour, desiredMinute))
+            return;
+
+        var minutes = 60 * (desiredHour - currentTime.Hour)
+            - currentTime.Minute
+            + desiredMinute;
+
+        await Task.Delay(TimeSpan.FromMinutes(minutes), stoppingToken);
+
+        if (onComplete != null)
+            await onComplete(stoppingToken);
+    }
+
+    private async static Task FinishDay(CancellationToken stoppingToken)
+    {
+        await MinutesUntil(desiredHour: 23, desiredMinute: 59, null, stoppingToken);
     }
 }
